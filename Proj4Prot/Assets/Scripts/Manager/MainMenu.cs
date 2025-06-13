17164b5af68d5ac57615eb9520882166a6a02117
Oguibecker <132964768+Oguibecker@users.noreply.github.com>
2025-06-12 23:56:53 -0300

    Menu de Opções pronto
    Falta fazer ele baixar o som e quando ativar o menu, pausar o áudio também

--- Proj4Prot/Assets/Scripts/Manager/MainMenu.cs
+++ Proj4Prot/Assets/Scripts/Manager/MainMenu.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
+    [Header("Menus")]
     public GameObject panelMainMenu;
     public GameObject panelOptions;
 
+    [Header("Configurações")]
+    public bool isStartMenu = false; 
+
+    private bool isPaused = false;
+
     private void Start()
     {
-        ShowMainMenu();
+        if (isStartMenu)
+        {
+            
+            Time.timeScale = 1f;
+            ShowMainMenu();
+        }
+        else
+        {
+            panelMainMenu.SetActive(false);
+            panelOptions.SetActive(false);
+        }
     }
 
+    private void Update()
+    {
+        if (!isStartMenu && Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    
     public void ShowMainMenu()
     {
         panelMainMenu.SetActive(true);
@@ -23,13 +52,36 @@ public class MainMenu : MonoBehaviour
         panelOptions.SetActive(true);
     }
 
+    
     public void PlayGame()
     {
-        SceneManager.LoadScene("Main");
+        if (isStartMenu)
+        {
+            SceneManager.LoadScene("Main"); 
+        }
+        else
+        {
+            ResumeGame();
+        }
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void PauseGame()
+    {
+        panelMainMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    private void ResumeGame()
+    {
+        panelMainMenu.SetActive(false);
+        panelOptions.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
     }
 }
